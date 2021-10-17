f4e938a12cbea0a049f282fc25fbfdf40600aff1
sleepycelery <chaoyihu@outlook.com>
2021-10-17 16:25:33 +0800

    Initial Commit

---
+++ build_requests.py
@@ -0,0 +1,195 @@
+from gevent import monkey
+
+monkey.patch_all()
+import gevent
+import requests
+from config import *
+from urllib3.util import parse_url
+import execjs
+from subprocess import Popen, PIPE
+import re
+import time
+
+
+def get_classinfo_dict():
+    result_list = []
+    response = requests.get(url=lesson_list_url, headers=homepage_headers)
+    data = response.json()['data']['list']
+    for i in data:
+        class_dict = {}
+        class_dict['course_name'] = i['course']['name']
+        class_dict['teacher_name'] = i['teacher']['name']
+        class_dict['classroom_id'] = i['classroom_id']
+        class_dict['course_id'] = i['course']['id']
+        result_list.append(class_dict)
+    return result_list
+
+
+def get_sku_id(classroom_id):
+    response = requests.get(url=get_skuid_url.format(classroom_id=classroom_id), headers=classroompage_headers)
+    return response.json()['data']['free_sku_id']
+
+
+def get_unfinished_video_id(sku_id, classroom_id):
+    score_detail_headers = classroompage_headers
+    score_detail_headers['classroom-id'] = str(classroom_id)
+    response = requests.get(url=score_detail_url.format(sku_id=sku_id), headers=score_detail_headers)
+    data = response.json()['data']['leaf_level_infos']
+    ids = []
+    for i in data:
+        info = {}
+        if i['leaf_type'] == 0 and i['schedule'] != 1:
+            info['name'] = i['leaf_level_title']
+            info['id'] = i['id']
+            ids.append(info)
+    return ids
+
+
+class VideoSession:
+    def __init__(self, classroom_id, video_id):
+        self.classroom_id = classroom_id
+        self.video_id = video_id
+        self.randomcode = execjs.eval("Math.floor(1048576 * (1 + Math.random())).toString(36)")
+        leaf_info_headers = videopage_headers
+        leaf_info_headers['classroom-id'] = str(self.classroom_id)
+        response = requests.get(url=leaf_info_url.format(classroom_id=self.classroom_id, video_id=self.video_id),
+                                headers=leaf_info_headers)
+
+        data = response.json()['data']
+        self.sku_id = data['sku_id']
+        self.course_id = data['course_id']
+        self.video_id = data['id']
+        self.user_id = data['user_id']
+        self.ccid = data['content_info']['media']['ccid']
+        self.classroom_id = data['classroom_id']
+        self.class_start_time = data['class_start_time']
+        self.class_end_time = data['class_end_time']
+
+    def get_play_domain(self):
+        playurl_headers = videopage_headers
+        playurl_headers['classroom-id'] = str(classroom_id)
+        response = requests.get(url=playurl_info_url.format(ccid=self.ccid), headers=playurl_headers)
+        data = response.json()['data']['playurl']['sources']
+        for i in data.keys():
+            return parse_url(data[i][0]).host
+
+    def get_video_watch_progress(self):
+        video_progress_headers = videopage_headers
+        video_progress_headers['classroom-id'] = str(classroom_id)
+        response = requests.get(
+            url=video_watch_progress_url.format(course_id=self.course_id, user_id=self.user_id,
+                                                classroom_id=self.classroom_id,
+                                                video_id=self.video_id), headers=video_progress_headers)
+        print(response.json())
+
+    def get_video_detail(self):
+        video_detail_headers = classroompage_headers
+        video_detail_headers['classroom-id'] = str(classroom_id)
+        response = requests.get(video_detail_url.format(classroom_id=self.classroom_id, video_id=self.video_id),
+                                headers=video_detail_headers)
+        print(response.json())
+
+    def get_play_url(self):
+        playurl_headers = videopage_headers
+        playurl_headers['classroom-id'] = str(classroom_id)
+        response = requests.get(url=playurl_info_url.format(ccid=self.ccid), headers=playurl_headers)
+        data = response.json()['data']['playurl']['sources']
+        for i in data.keys():
+            return data[i][0]
+
+    def get_video_length(self):
+        play_url = self.get_play_url()
+        p = Popen('./ffprobe.exe -print_format json {}'.format(play_url), stdout=PIPE, stderr=PIPE, stdin=PIPE)
+        p.wait()
+        output = p.communicate()[1].decode()
+        pattern = 'Duration: (.*?),'
+        time_string = re.findall(pattern, output)[0]
+        h, m, s = time_string.split(':')
+        time = int(h) * 3600 + int(m) * 60 + round(float(s), 1)
+        return time
+
+    def _build_heartbeat_packets(self):
+        sq_count = 1
+        video_duration = self.get_video_length()
+        play_domain = self.get_play_domain()
+        magic_time = 4.7
+
+        def build_payload(played_time: float, video_duration: float, type: str, sq: int, timestamp: int):
+            return {
+                'c': self.course_id,
+                'cc': self.ccid,
+                'classroomid': self.classroom_id,
+                'cp': played_time,
+                'd': video_duration,
+                'et': type,
+                'fp': 0,
+                'i': 5,
+                'lob': 'ykt',
+                'n': play_domain,
+                'p': 'web',
+                'pg': '{}_{}'.format(self.video_id, self.randomcode),
+                'skuid': self.sku_id,
+                'sp': 1,
+                'sq': sq,
+                't': 'video',
+                'tp': 0,
+                'ts': str(timestamp),
+                'u': self.user_id,
+                'uip': "",
+                'v': self.video_id
+            }
+
+        payloads = []
+        payloads.append(build_payload(played_time=0, video_duration=0, type='loadstart', sq=sq_count,
+                                      timestamp=int(time.time() * 1000)))
+        sq_count += 1
+        payloads.append(build_payload(played_time=0, video_duration=video_duration, type='loadeddata', sq=sq_count,
+                                      timestamp=int(time.time() * 1000)))
+        sq_count += 1
+        payloads.append(build_payload(played_time=0, video_duration=video_duration, type='play', sq=sq_count,
+                                      timestamp=int(time.time() * 1000)))
+        sq_count += 1
+        payloads.append(build_payload(played_time=0, video_duration=video_duration, type='playing', sq=sq_count,
+                                      timestamp=int(time.time() * 1000)))
+        sq_count += 1
+        for i in range(int((video_duration - magic_time) / 5) + 5):
+            if magic_time + i * 5 <= video_duration:
+                payloads.append(
+                    build_payload(played_time=magic_time + i * 5, video_duration=video_duration, type='heartbeat',
+                                  sq=sq_count,
+                                  timestamp=int(time.time() * 1000)))
+                sq_count += 1
+            else:
+                break
+        payloads.append(
+            build_payload(played_time=video_duration, video_duration=video_duration, type='videoend', sq=sq_count,
+                          timestamp=int(time.time() * 1000)))
+        return payloads
+
+    def send_heartbeats(self, coroutine=False):
+        payloads = self._build_heartbeat_packets()
+        pos = 0
+        heartbeat_data_list = []
+        for i in range(int(len(payloads) / 5)):
+            heartbeat_data = {'heart_data': payloads[pos:pos + 5]}
+            heartbeat_data_list.append(heartbeat_data)
+            pos += 5
+        heartbeat_data_list.append({'heart_data': payloads[pos:]})
+
+        def send_packet(json_data):
+            requests.post(url=heartbeat_url, headers=heartbeat_headers, json=json_data)
+
+        if not coroutine:
+            for i in heartbeat_data_list:
+                send_packet(i)
+        else:
+            gevent.joinall([gevent.spawn(send_packet, i) for i in heartbeat_data_list])
+
+
+if __name__ == '__main__':
+    classinfo = get_classinfo_dict()
+    classroom_id = classinfo[0]['classroom_id']
+    sku_id = get_sku_id(classroom_id)
+    video_ids = get_unfinished_video_id(sku_id, str(classroom_id))
+    session = VideoSession(classroom_id, video_ids[0]['id'])
+    session.send_heartbeats(coroutine=True)
---
+++ config.py
@@ -0,0 +1,101 @@
+cookie = ''
+login_url = 'https://changjiang.yuketang.cn/v2/web/index'
+lesson_list_url = 'https://changjiang.yuketang.cn/v2/api/web/courses/list?identity=2'
+get_skuid_url = 'https://changjiang.yuketang.cn/v2/api/web/classrooms/{classroom_id}?role=5'
+score_detail_url = 'https://changjiang.yuketang.cn/c27/online_courseware/schedule/score_detail/single/{sku_id}/0/'
+leaf_info_url = 'https://changjiang.yuketang.cn/mooc-api/v1/lms/learn/leaf_info/{classroom_id}/{video_id}/'
+playurl_info_url = 'https://changjiang.yuketang.cn/api/open/audiovideo/playurl?video_id={ccid}&provider=cc&file_type=1&is_single=0&domain=changjiang.yuketang.cn'
+video_watch_progress_url = 'https://changjiang.yuketang.cn/video-log/get_video_watch_progress/?cid={course_id}&user_id={user_id}&classroom_id={classroom_id}&video_type=video&vtype=rate&video_id={video_id}&snapshot=1'
+heartbeat_url = 'https://changjiang.yuketang.cn/video-log/heartbeat/'
+video_detail_url = 'https://changjiang.yuketang.cn/video-log/detail/?classroom_id={classroom_id}&user_id=0&video_id={video_id}'
+
+
+def cookie_str2dict(cookiestr):
+    cookie_dict = {}
+    cookie_list = cookiestr.split(';')
+    for i in cookie_list:
+        key, value = i.strip().split('=')
+        cookie_dict[key] = value
+    return cookie_dict
+
+
+homepage_headers = {
+    'authority': 'changjiang.yuketang.cn',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+    'dnt': '1',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'xt-agent': 'web',
+    'accept': 'application/json, text/plain, */*',
+    'xtbz': 'ykt',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://changjiang.yuketang.cn/v2/web/index',
+    'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6',
+    'cookie': cookie
+}
+
+classroompage_headers = {
+    'authority': 'changjiang.yuketang.cn',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+    'x-client': 'web',
+    'dnt': '1',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'xt-agent': 'web',
+    'accept': 'application/json, text/plain, */*',
+    'xtbz': 'ykt',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6',
+    'cookie': cookie
+}
+
+videopage_headers = {
+    'authority': 'changjiang.yuketang.cn',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+    'dnt': '1',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'xt-agent': 'web',
+    'accept': 'application/json, text/plain, */*',
+    'xtbz': 'ykt',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6',
+    'cookie': cookie
+}
+
+heartbeat_headers = {
+    'authority': 'changjiang.yuketang.cn',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+    'dnt': '1',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'content-type': 'application/json',
+    'accept': '*/*',
+    'x-requested-with': 'XMLHttpRequest',
+    'xtbz': 'ykt',
+    'x-csrftoken': cookie_str2dict(cookie)['csrftoken'],
+    'sec-ch-ua-platform': '"Windows"',
+    'origin': 'https://changjiang.yuketang.cn',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6',
+    'cookie': cookie
+}
